ade48d064240f38a33c04a8afb033cf565eba0a6
Marcio Soster Arrosi <marcio.arrosi@procempa.com.br>
2017-10-27 16:58:58 -0200

    sample adicionar login firebase

--- app/src/main/java/br/com/gearsoft/doarsangue/MainActivity.java
+++ app/src/main/java/br/com/gearsoft/doarsangue/MainActivity.java
@@ -1,7 +1,10 @@
 package br.com.gearsoft.doarsangue;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
@@ -16,6 +19,14 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -41,6 +52,11 @@ public class MainActivity extends AppCompatActivity implements OnSolicitacaoInte
      */
     private SectionsPagerAdapter mSectionsPagerAdapter;
 
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int RC_SIGN_IN = 8907; // max value 65.535
+    private FirebaseAuth mFirebaseAuth;
+    private boolean mIsSigningIn;
+
     /**
      * The {@link ViewPager} that will host the section contents.
      */
@@ -55,6 +71,42 @@ public class MainActivity extends AppCompatActivity implements OnSolicitacaoInte
 
     @BindView(R.id.fab)
     FloatingActionButton fab;
+
+    @BindView(R.id.main_content)
+    View mRootView;
+
+    private boolean shouldStartSignIn() {
+        return (!mIsSigningIn && mFirebaseAuth.getCurrentUser() == null);
+    }
+
+    private void startSignIn() {
+        Intent intent =
+                AuthUI.getInstance().createSignInIntentBuilder()
+                        .setAvailableProviders(Arrays.asList(
+                                new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                                new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build())
+                        )
+                        .setIsSmartLockEnabled(!BuildConfig.DEBUG)// desabilita em dev habilita em prod
+                        .build();
+        startActivityForResult(intent, RC_SIGN_IN);
+        mIsSigningIn = true;
+    }
+
+    private void signOut() {
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    public void onComplete(@NonNull Task<Void> task) {
+                        showSnackbar("Logouted");
+                    }
+                });
+
+    }
+
+    @MainThread
+    private void showSnackbar(String errorMessageRes) {
+        Snackbar.make(mRootView, errorMessageRes, Snackbar.LENGTH_LONG).show();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -77,10 +129,20 @@ public class MainActivity extends AppCompatActivity implements OnSolicitacaoInte
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Deve ir para uma atividade de criação", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                signOut();
             }
         });
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (shouldStartSignIn()) {
+            startSignIn();
+        }
     }
 
     @Override
@@ -106,18 +168,49 @@ public class MainActivity extends AppCompatActivity implements OnSolicitacaoInte
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            mIsSigningIn = false;
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Sign in failed
+            if (resultCode != RESULT_OK && shouldStartSignIn()) {
+                if (response == null) {
+                    // User pressed back button
+                    showSnackbar("sign_in_cancelled");
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    showSnackbar("no_internet_connection");
+                    return;
+                }
+
+                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
+                    showSnackbar("unknown_error");
+                    return;
+                }
+
+                showSnackbar("unknown_sign_in_response");
+            }
+        }
+    }
+
+    @Override
     public void onClickDoacaoItem(Doacao doacao) {
-        Log.d("CLICK", "onClickDoacaoItem: " + doacao.toString());
+        Log.d(TAG, "onClickDoacaoItem: " + doacao.toString());
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
-        Log.d("CLICK", "onClickPerfil");
+        Log.d(TAG, "onClickPerfil");
     }
 
     @Override
     public void onClickSolicitacaoItem(Solicitacao solicitacao) {
-        Log.d("CLICK", "onClickSolicitacaoItem: " +solicitacao.toString());
+        Log.d(TAG, "onClickSolicitacaoItem: " +solicitacao.toString());
 
     }
 
--- app/src/main/java/br/com/gearsoft/doarsangue/domain/Pessoa.java
+++ app/src/main/java/br/com/gearsoft/doarsangue/domain/Pessoa.java
@@ -1,6 +1,8 @@
 package br.com.gearsoft.doarsangue.domain;
 
-import java.sql.Date;
+import com.google.firebase.firestore.Exclude;
+
+import java.util.Date;
 import java.util.List;
 
 import br.com.gearsoft.doarsangue.domain.enums.FatorRH;
@@ -14,12 +16,66 @@ public class Pessoa {
 
     private FatorRH fatorRH;
     private TipoSanguineo tipoSanguineo;
-    private String nome;
     private String primeiroNome;
+    private String segundoNome;
     private String ultimoNome;
     private Date ultimaDoacao;
+    @Exclude
     private List<Doacao> historicoDoacoes;
 
+    public FatorRH getFatorRH() {
+        return fatorRH;
+    }
 
+    public void setFatorRH(FatorRH fatorRH) {
+        this.fatorRH = fatorRH;
+    }
 
+    public TipoSanguineo getTipoSanguineo() {
+        return tipoSanguineo;
+    }
+
+    public void setTipoSanguineo(TipoSanguineo tipoSanguineo) {
+        this.tipoSanguineo = tipoSanguineo;
+    }
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSegundoNome() {
+        return segundoNome;
+    }
+
+    public void setSegundoNome(String segundoNome) {
+        this.segundoNome = segundoNome;
+    }
+
+    public String getUltimoNome() {
+        return ultimoNome;
+    }
+
+    public void setUltimoNome(String ultimoNome) {
+        this.ultimoNome = ultimoNome;
+    }
+
+    public Date getUltimaDoacao() {
+        return ultimaDoacao;
+    }
+
+    public void setUltimaDoacao(Date ultimaDoacao) {
+        this.ultimaDoacao = ultimaDoacao;
+    }
+
+    public List<Doacao> getHistoricoDoacoes() {
+        return historicoDoacoes;
+    }
+
+    public void setHistoricoDoacoes(List<Doacao> historicoDoacoes) {
+        this.historicoDoacoes = historicoDoacoes;
+    }
 }
